26e48674115e6a8fc2660e9cc53fdb33c23b8efa
booba-magistr <mainpo4ta1337@mail.ru>
2024-10-21 13:21:36 +0300

    add admin file

--- gadalka/main/admin.py
+++ gadalka/main/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import MainModel
 
 # Register your models here.
+@admin.register(MainModel)
+class MainAdmin(admin.ModelAdmin):
+    fields = 'question', 'get_answer', 'time_create'
+    list_display = 'pk', 'question', 'get_answer', 'time_create'
+    readonly_fields = 'time_create', 'get_answer', 'question'
+    ordering = 'pk',
+    list_per_page = 20
+    search_fields = 'question',
+    list_display_links = 'question', 
+    
+    @admin.display(description='Ответ')
+    def get_answer(self, model:MainModel):
+        if model.answer:
+            return 'Да'
+        return 'Нет'
